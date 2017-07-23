7f1dcc00d46dc867e5aeb883bc9922105d61e581
Jenny Chen <jennycjk1201@gmail.com>
2017-07-23 14:54:41 -0700

    change map url https to http

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ function loadData() {
     var streetStr = $('#street').val();
     var cityStr = $('#city').val();
     var address = streetStr + ', ' + cityStr;
-    var streetviewUrl = 'https://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address;
+    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address;
     $greeting.text('So, you want to live at '+ address + '?');
     $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
 
